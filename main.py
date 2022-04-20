9007da24693f0e8d711b7a8d61fddab03742cdb2
Eduard Kozhukhov <eduardbeliy@mail.ru>
2022-04-20 21:51:50 +0300

    Homework completed

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ import utils
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    pass
+@app.route('/')  # Главная страница с кандидатами
+def page_main():
+    candidate_list = utils.load_candidates("candidates.json")
+
+    return utils.output_candidates(candidate_list)
 
 
-@app.route('/candidate/<candidate_id>')
+@app.route('/candidate/<int:candidate_id>')  # Страница кандидата
 def candidate(candidate_id):
-    pass
+    candidate_list = utils.load_candidates("candidates.json")
+    candidate = utils.get_candidate_by_id(candidate_list, candidate_id)
+    result = f"<img src={candidate['picture']}>"
+
+    return result + utils.output_candidates([candidate])
 
 
-@app.route('/skills/<skill>')
-def skills(skill):
-    pass
+@app.route('/skills/<candidate_skill>')  # Страница кандидатов с нужным скилом
+def skills(candidate_skill):
+    candidate_list = utils.load_candidates("candidates.json")
+    return utils.output_candidates(utils.get_candidates_by_skill(candidate_list, candidate_skill))
+
+
+app.run()
--- utils.py
+++ utils.py
@@ -1,8 +1,47 @@
 import json
 
 
-def load_candidates():
-    with open("candidates.json", "r", encoding="utf=8") as file:
+def load_candidates(path):
+    """ Загружает из файла JSON кандидатов и возвращает их """
+
+    with open(path, "r", encoding="utf=8") as file:
         candidates = json.load(file)
         return candidates
 
+
+def output_candidates(candidates_list):
+    """
+    Выводит список кандидатов в формате:
+    Имя кандидата:
+    Позиция кандидата:
+    Навыки:
+    """
+
+    result = "<pre>"
+    for candidate in candidates_list:
+        result += (
+            f"Имя кандидата: {candidate['name']}\n"
+            f"Позиция кандидата: {candidate['position']}\n"
+            f"Навыки: {candidate['skills']}\n\n"
+        )
+    result += "<pre>"
+    return result
+
+
+def get_candidate_by_id(candidates_list, candidate_id):
+    """ Возвращает кандидата по его id """
+
+    for candidate in candidates_list:
+        if candidate["id"] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_skill(candidates_list, candidate_skill):
+    """ Возвращает список кандидатов по наличию скилов """
+
+    result = []
+    for candidate in candidates_list:
+        candidate_skills = candidate["skills"].lower().split(", ")
+        if candidate_skill.lower() in candidate_skills:
+            result.append(candidate)
+    return result
